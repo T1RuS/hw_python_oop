184e71fa9e13788ace575cd79a336d0f7290c517
Михутов Тимур <tumka5688g@gmail.com>
2022-04-01 16:57:54 +0300

    Все готово

--- homework.py
+++ homework.py
@@ -1,13 +1,32 @@
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    pass
+
+    def __init__(self, training_type: str,
+                 duration: float,
+                 distance: float,
+                 speed: float,
+                 calories: float) -> None:
+        self.training_type = training_type
+        self.duration = duration
+        self.distance = distance
+        self.speed = speed
+        self.calories = calories
+
+    def get_message(self) -> str:  # f"{numObj:.{digits}f}" не особо понял как работает
+        # данная конструкция для обрезаний чисел. Можете рассказать?
+        return f"Тип тренировки: {self.training_type}; " \
+               f"Длительность: {self.duration:.{3}f} ч.; " \
+               f"Дистанция: {self.distance:.{3}f} км; " \
+               f"Ср. скорость: {self.speed:.{3}f} км/ч; " \
+               f"Потрачено ккал: {self.calories:.{3}f}."
 
 
 class Training:
     """Базовый класс тренировки."""
-
-    LEN_STEP: float = 1
+    NAME_OF_TRAINING = 'тренировка'
+    LEN_STEP: float = 0.65
     M_IN_KM: float = 1000
+    HOURS_PER_MINUTE: float = 60
 
     def __init__(self,
                  action: int,
@@ -24,7 +43,7 @@ class Training:
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        pass
+        return self.get_distance() / self.duration
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
@@ -32,40 +51,104 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        pass
+        train = InfoMessage(self.NAME_OF_TRAINING, self.duration, self.get_distance(),
+                            self.get_mean_speed(), self.get_spent_calories())
+        return train
 
 
 class Running(Training):
     """Тренировка: бег."""
-
-    LEN_STEP: float = 0.65
-
-    def __init__(self, action: int, duration: float, weight: float) -> None:
-        super().__init__(action, duration, weight)
+    NAME_OF_TRAINING = 'Running'
 
     def get_distance(self) -> float:
         """Получить дистанцию в км для бега."""
         return super().get_distance()
 
+    def get_spent_calories(self) -> float:
+        """Получить количество затраченных калорий для бега."""
+        coeff_calorie_1 = 18
+        coeff_calorie_2 = 20  # почему в строке 68 нужен косой слеш при переносе, а в строке 95 нет?
+        return (coeff_calorie_1 * self.get_mean_speed() - coeff_calorie_2) * \
+               self.weight / self.M_IN_KM * self.duration * self.HOURS_PER_MINUTE
+
+    def show_training_info(self) -> InfoMessage:
+        return super().show_training_info()
+
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-    pass
+    NAME_OF_TRAINING = 'SportsWalking'
+
+    def __init__(self,
+                 action: int,
+                 duration: float,
+                 weight: float,
+                 height: float) -> None:
+        super().__init__(action, duration, weight)
+        self.height = height
+
+    def get_distance(self) -> float:
+        """Получить дистанцию в км для ходьбы."""
+        return super().get_distance()
+
+    def get_spent_calories(self) -> float:
+        """Получить количество затраченных калорий для бега."""
+        coeff_calorie_1: float = 0.035
+        coeff_calorie_2: float = 0.029
+        return (coeff_calorie_1 * self.weight + (self.get_mean_speed() ** 2 //
+                self.height) * coeff_calorie_2 * self.weight) * self.duration * self.HOURS_PER_MINUTE
+
+    def show_training_info(self) -> InfoMessage:
+        return super().show_training_info()
 
 
 class Swimming(Training):
     """Тренировка: плавание."""
-    pass
+    LEN_STEP: float = 1.38
+    NAME_OF_TRAINING = 'Swimming'
+
+    def __init__(self,
+                 action: int,
+                 duration: float,
+                 weight: float,
+                 length_pool: float,
+                 count_pool: float,
+                 ) -> None:
+        """Добавить длину бассейна."""
+        super().__init__(action, duration, weight)
+        self.length_pool = length_pool
+        self.count_pool = count_pool
+
+    def get_distance(self) -> float:
+        """Получить дистанцию в км для плавания."""
+        return super().get_distance()
+
+    def get_mean_speed(self) -> float:
+        """Получить среднюю скорость движения в бассейне."""
+        return self.length_pool * self.count_pool / self.M_IN_KM / self.duration
+
+    def get_spent_calories(self) -> float:
+        """Получить количество затраченных калорий."""
+        return (self.get_mean_speed() + 1.1) * 2 * self.weight
+
+    def show_training_info(self) -> InfoMessage:
+        return super().show_training_info()
 
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
-    pass
+    type_of_training = {
+        'SWM': Swimming,
+        'RUN': Running,
+        'WLK': SportsWalking
+    }
+    return type_of_training[workout_type](*data)
 
 
 def main(training: Training) -> None:
     """Главная функция."""
-    pass
+    info = training.show_training_info()
+    print(info.get_message())
 
 
 if __name__ == '__main__':
@@ -78,4 +161,3 @@ if __name__ == '__main__':
     for workout_type, data in packages:
         training = read_package(workout_type, data)
         main(training)
-
