3db2bdb7a2255b53ccbdda8e8482b7b0c9a85e27
Михутов Тимур <tumka5688g@gmail.com>
2022-04-01 22:54:20 +0300

    Отредактировал строки.

--- homework.py
+++ homework.py
@@ -53,12 +53,12 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        training = InfoMessage(self.NAME_OF_TRAINING,
-                            self.duration,
-                            self.get_distance(),
-                            self.get_mean_speed(),
-                            self.get_spent_calories())
-        return training
+        text = InfoMessage(self.NAME_OF_TRAINING,
+                           self.duration,
+                           self.get_distance(),
+                           self.get_mean_speed(),
+                           self.get_spent_calories())
+        return text
 
 
 class Running(Training):
